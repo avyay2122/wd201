dc8cf520f8dde9119c74bca73bd26ee87db2682c
Avyay <avyay2122@gmail.com>
2023-12-04 19:51:44 +0530

    add lint staged

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,32 +1,31 @@
-const todoList = require('../todo');
+/* eslint-disable no-undef */
+const todoList = require("../todo");
 
-const {all, markAsComplete, add} = todoList();
+const { all, markAsComplete, add } = todoList();
 
-describe("Todolist Test Suite", () => {
-    beforeAll(() => {
-        add(
-            {
-                title: "Test todo",
-                completed: false,
-                dueDate: new Date().toISOString()
-            }
-        );
-    })
-    test("Should add new todo", () => {
-        const todoItemsCount = all.length;
-        add(
-            {
-                title: "Test todo",
-                completed: false,
-                dueDate: new Date().toISOString()
-            }
-        );
-        expect(all.length).toBe(todoItemsCount + 1);
+describe("TodoList Test Suite", () => {
+  beforeAll(() => {
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
     });
-
-    test("Should mark a todo as complete", () =>{
-        expect(all[0].completed).toBe(false);
-        markAsComplete(0);
-        expect(all[0].completed).toBe(true);
-    })
-})
+  });
+  test("Should add new todo", () => {
+    const todoItemsCount = all.length;
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    expect(all.length).toBe(todoItemsCount + 1);
+  });
+  test("should mark todo as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+  test("checks retrieval of overdue items", () => {
+    expect();
+  });
+});
--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const todoList = () => {
   let currentDate = new Date();
   let year = currentDate.getFullYear();
