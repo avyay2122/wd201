82f75baf64c49f5c72f39b56a461e2fa991acc6c
Avyay <avyay2122@gmail.com>
2023-12-05 21:33:50 +0530

    ---

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,7 +1,28 @@
 /* eslint-disable no-undef */
 const todoList = require("../todo");
 
-const { all, markAsComplete, add } = todoList();
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
+
+const today = new Date();
+let yesterday = new Date(today);
+yesterday.setDate(today.getDate() - 1);
+
+yesterday =
+  yesterday.getFullYear() +
+  "-" +
+  String(yesterday.getMonth() + 1).padStart(2, "0") +
+  "-" +
+  String(yesterday.getDate()).padStart(2, "0");
+
+let tmrw = new Date(today);
+tmrw.setDate(today.getDate() + 1);
+
+tmrw =
+  tmrw.getFullYear() +
+  "-" +
+  String(tmrw.getMonth() + 1).padStart(2, "0") +
+  "-" +
+  String(tmrw.getDate()).padStart(2, "0");
 
 describe("TodoList Test Suite", () => {
   beforeAll(() => {
@@ -9,6 +30,16 @@ describe("TodoList Test Suite", () => {
       title: "Test todo",
       completed: false,
       dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: yesterday,
+    });
+    add({
+      title: "Test todo",
+      completed: false,
+      dueDate: tmrw,
     });
   });
   test("Should add new todo", () => {
@@ -25,7 +56,21 @@ describe("TodoList Test Suite", () => {
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
   });
-  test("checks retrieval of overdue items", () => {
-    expect();
+  test("should retrieve overdue items", () => {
+    const overdueItems = overdue();
+
+    expect(overdueItems.length).toBe(1);
+  });
+
+  test("should retrieve duelater items", () => {
+    const dueLaters = dueLater();
+
+    expect(dueLaters.length).toBe(1);
+  });
+
+  test("should retrieve duetoday items", () => {
+    const dueTodays = dueToday();
+
+    expect(dueTodays.length).toBe(2);
   });
 });
